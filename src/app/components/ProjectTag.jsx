e0cacfa08e802e2675cf1414839c5ffdf23066c7
samarpanpdl <poudelaashutosh27@gmail.com>
2025-12-11 19:19:41 +0545

    updates

---
+++ src/app/components/ProjectTag.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectTag = ({name,onClick, isSelected}) => {
+    const buttonStyles= isSelected ? 'border-purple-500 text-white'
+     : 'text-[#ADB7BE] border-slate-600 hover:border-white';
+  return (
+        <button className={`${buttonStyles} rounded-full text-white border-2 px-6 py-3 text-xl cursor-pointer`}
+            onClick={()=>onClick(name)}
+        >
+            {name}
+            </button>
+
+  )
+}
+
+export default ProjectTag
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import UpSection from "./components/UpSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col bg-[#121212] ">
@@ -9,6 +10,7 @@ export default function Home() {
         <div className="container mx-auto mt-24 px-12 py-4">
         <UpSection/>
         <AboutSection/>
+        <ProjectsSection/>
         </div>
       </main>
   );
