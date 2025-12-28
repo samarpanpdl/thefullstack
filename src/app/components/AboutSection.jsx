63d165228093de59098ef92b697694b063773993
samarpanpdl <poudelaashutosh27@gmail.com>
2025-12-28 20:39:39 +0545

    semifinal update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,8 +1,8 @@
 "use client"
-import React,{useState,useTransition} from 'react'
+import React,{useState,useTransition,useRef} from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton';
-
+import {motion,useInView} from "framer-motion";
 const TAB_DATA = [
     {
         title: 'Skills',
@@ -46,6 +46,8 @@ const TAB_DATA = [
 ]
 
 const AboutSection = () => {
+    const ref = useRef(null);
+    const isinView = useInView(ref,{once:true});
     const [tab,setTab]=useState('skills');
     const [isPending,startTransition]=useTransition();
 
@@ -55,13 +57,22 @@ const AboutSection = () => {
         })
 
     }
+    const cardVariants = {
+      initial: {y:50,opacity:0},
+      animate: {y:0,opacity:1}
+    }
   return (
-    <section className='text-white' id='about'>
-        <div className='md: grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+    
+    <section ref={ref} className='text-white' id='about'>
+        <motion.div 
+            variants={cardVariants} initial="initial" animate={isinView ? "animate" : "initial"} transition={{duration:2.0}}
+        className='md: grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
         <Image src="/images/about-image.png" alt="about image" width={500} height={500} className='rounded-lg'/>
         <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
             <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
-            <p className='text-base md:text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus accusantium, vero pariatur ipsa necessitatibus eum! Necessitatibus libero, dolorum cupiditate temporibus officia atque quae, optio in minima sint magni facilis ullam, quis explicabo.</p>
+            <p className='text-base md:text-lg'>
+                I am a passionate developer with a strong foundation in Python, Django, and web development. I enjoy creating efficient and scalable solutions to complex problems. With a keen eye for detail and a commitment to high-quality code, I strive to deliver innovative applications that meet user needs and exceed expectations.
+                </p>
             <div className='flex flex-row justify-start mt-8'>
                 <TabButton selectTab={()=> handleTabChange("skills")} active={tab==="skills"}>{" "}Skills{" "}</TabButton>
                 <TabButton selectTab={()=> handleTabChange("education")} active={tab==="education"}>{" "}Education{" "}</TabButton>
@@ -71,7 +82,7 @@ const AboutSection = () => {
                 {TAB_DATA.find((t)=> t.id.toLowerCase() === tab.toLowerCase())?.content}
             </div>
         </div>
-        </div>
+        </motion.div>
     </section>
   )
 }
